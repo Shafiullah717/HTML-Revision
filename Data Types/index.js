87becf7c4e00201485387149f36585edac8d22da
Developer-Shafi <shafitechdeveloper1133@gmail.com>
2025-02-03 23:20:19 +0500

    just learn datatypes and logical operators

---
+++ Data Types/index.js
@@ -0,0 +1,47 @@
+// console.log("Muhammad Shafiullah")
+// //Data Types in JS
+// //number
+// //string
+// //boolean
+// //null
+// //undefined
+// //objectf
+
+// let value = 20
+// console.log(typeof(value))
+
+// let firstName = "Muhammad"
+// console.log(typeof(firstName))
+
+// let userloggedin = true
+// console.log(typeof(userloggedin))
+// if(0){
+//     console.log("Sucessfull")
+// }
+
+// let number = null
+
+// const student ={
+//     name : "Shafiullah",
+//     age : 21,
+//     education : "Graduate",
+// }
+// console.log(student)
+// console.log(student.age)
+
+// And
+// Or 
+// Not 
+// let age = 13
+// let critereya = 22
+let name = "Ahsan"
+// if(age >= critereya || name == "shafiullah"){
+//     console.log("you can vote!")
+// }
+if(name != "Ahsan"){
+    console.log("Sucessfull")
+}
+
+
+
+
