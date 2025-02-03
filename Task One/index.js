d7d6394260c87ac2e47d39049a489ffaab393106
Developer-Shafi <shafitechdeveloper1133@gmail.com>
2025-02-03 22:29:59 +0500

    Variables revision

---
+++ Task One/index.js
@@ -0,0 +1,29 @@
+console.log(
+    "Ahsan and shafiullah"
+)
+
+// variables
+// var global 
+// var name = "Ahsan"
+// console.log(name)
+// name = "Shafiullah"
+// console.log(name)
+// if(true){
+//     var value = 2
+// console.log(value)
+// }
+
+// console.log(value)
+let value =10
+console.log(value)
+value = 40
+console.log(value)
+
+if(true){
+    let name = "Ahmad"
+}
+console.log(name)
+
+const value2 = 20
+value2 = 22
+console.log(value2)
