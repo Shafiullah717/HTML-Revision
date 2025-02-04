6962057bd7127fdc24ebb398a49163e474f3bddd
Developer-Shafi <shafitechdeveloper1133@gmail.com>
2025-02-04 23:04:02 +0500

    learn If else and for

--- 4FOUR.js
+++ 4FOUR.js
@@ -1,1 +1,31 @@
 console.log("Muhammad Shafiullah")
+// if || else conditions
+// const nimage = 17;
+// const ward = 26;
+// if(nimage >= 18){
+//     console.log("You can vote")
+// }else if(ward == 26){
+//     console.log("You can vote")
+// }else{
+//     console.log("Aap ke vote ka koi fiada nahi")
+// }
+
+// Switch
+// const options = 3;
+// switch(options){
+//     case 1 : {
+//         console.log("Urdu")
+//     }
+//     break;
+//     case 2 :{
+//         console.log("English")
+//     }
+//     break;
+//     case 3 :{
+//         console.log("Chines")
+//     }
+//     break
+// }
+for(let i = 1; i <= 10; i=i+1){
+    console.log("Ahsan",i)
+}
